9a09baa786469ef4085478d41c80eae3e1127d69
Rashmita_Paul <rashmitapaul2@gmail.com>
2024-03-27 16:20:55 +0530

    I am pushing code.

--- src/test/java/org/example/BuilderPatternDesignPatternJava.java
+++ src/test/java/org/example/BuilderPatternDesignPatternJava.java
@@ -1,20 +1,25 @@
 package org.example;
 
-public class BuilderPatternDesignPattern {
+public class BuilderPatternDesignPatternJava {
     //Change return type of each method as class type"this" always points to current/calling objects.
     //Returning the same to have same reference.
 
 
-    public BuilderPatternDesignPattern stag1(){
+    public BuilderPatternDesignPatternJava stag1(){
         System.out.println("Stag 1");
         return this;
     }
-    public BuilderPatternDesignPattern stag2(String str){
-        System.out.println("Stag 1");
+    public BuilderPatternDesignPatternJava stag2(String str){
+        System.out.println("Stag 2 string is " + str);
         return this;
     }
-    public BuilderPatternDesignPattern stag1(){
-        System.out.println("Stag 1");
+    public BuilderPatternDesignPatternJava stag3(){
+        System.out.println("Stag 3");
         return this;
+    }
+
+    public static void main(String[] args) {
+        BuilderPatternDesignPatternJava bp = new BuilderPatternDesignPatternJava();
+        bp.stag1().stag2( "test").stag3();//Method chaining or BuilderPattern
     }
 }
--- src/test/java/org/example/RATestCase.java
+++ src/test/java/org/example/RATestCase.java
@@ -1,2 +1,49 @@
-package org.example;public class RATestCase {
+package org.example;
+
+import io.restassured.RestAssured;
+
+public class RATestCase {
+    public static void main(String[] args) {
+        //Need
+        //1.Get request -http://api.zippoptam.us/IN/757001
+        //2.URL -no
+        //3.HEADERS -NO
+        //4.Get Method -Yes
+        //5.base uri - http://api.zippoptam.us
+        //6.base path -/IN/757001
+        //7.payload - no
+        //Authentication - Basic, Digest, JWT,Bearer Token,OAuth 2.0 - no
+
+
+        //What you want to validate? -status code ,Response body, Time ,Headers ,Cookies
+
+        //Gherkin syn text --> Given, when , then -> it is just a style of BBD not a BBD framework
+
+        //RestAssured.given().when().then() -DSL(Domain specific language)(in java only)
+
+        //given() ->under come(URL,Headers,base url,base path,Authentication)
+
+        //when() ->under come(payload,Get Method)
+
+        //then() ->under come(Response,validation ,status code ,Response body, Time ,Headers ,Cookies)
+
+        RestAssured
+                .given()
+                .baseUri("http://api.zippoptam.us").basePath("/IN/757001")
+
+                .when()
+                .get()
+
+                .then().statusCode(200);
+
+        RestAssured
+                .given()
+                .baseUri("http://api.zippoptam.us").basePath("/IN/757001")
+
+                .when().log().all()
+                .get()
+
+                .then().log().all().statusCode(201);
+
+    }
 }
